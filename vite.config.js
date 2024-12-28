7633242dfcdf8f71055b6c59adbd41eda0ba5b67
Ashraf <icerafsyahmie12@gmail.com>
2024-12-28 12:51:14 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
-<<<<<<< Updated upstream
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
->>>>>>> Stashed changes
 
 export default defineConfig({
   plugins: [react()],
   build: {
-<<<<<<< Updated upstream
-    outDir: 'dist',
-=======
     outDir: "dist", // Default output directory
->>>>>>> Stashed changes
   },
 });
